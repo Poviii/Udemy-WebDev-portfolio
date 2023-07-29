7932533f46202c7876658152632a4f363ff50346
Povilas <paulsirut@gmail.com>
2023-07-29 14:30:09 +0200

    start on js

---
+++ JS/timeLeftCalc.js
@@ -0,0 +1,8 @@
+function lifeTime(age) {
+    var months = (90-age)*12;
+    var weeks = (90-age)*52;
+    var days = (90-age)*365;
+    console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.")
+}
+lifeTime(20)
+    
